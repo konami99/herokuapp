8145d9172c7ef5652bc4f314906fe9496b633d4b
rchou <konami99@hotmail.com>
2018-04-04 00:25:08 +1000

    move background job

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -1,5 +1,9 @@
 class WelcomeController < ApplicationController
   def index
+
+  end
+
+  def show
     TestJob.perform_later
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  get '/sidekiq', to: 'welcome#show'
+
   root 'welcome#index'
 end
